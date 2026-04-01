d2e2172dddaee5442c533a7154376fbccf768578
Sanskriti757 <151363795+Sanskriti757@users.noreply.github.com>
2026-04-01 19:25:26 +0530

    Add database connection setup with SQLAlchemy
    Implement database connection using SQLAlchemy with a session management function.

---
+++ backend/app/database/connection.py
@@ -0,0 +1,19 @@
+# ye file database connection ke liye hai, jisme hum sqlalchemy ka use karenge database se connect hone ke liye. Humne yaha pe ek function banaya hai get_db() jo hume database session provide karega, jise hum apne API endpoints me use kar sakte hain.
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import settings
+
+Base = declarative_base()
+
+engine = create_engine(settings.DB_CONNECTION)
+
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
