af541fc25356a3d1cdd439c3356772febf077416
Sanskriti757 <151363795+Sanskriti757@users.noreply.github.com>
2026-04-01 19:19:31 +0530

    Initialize fastapi with patient routes and database setup

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.database.connection import Base, engine
+
+from app.models.patient import PatientModel
+
+from app.routes.patient_routes import patient_router
+
+Base.metadata.create_all(engine)
+
+app=FastAPI(title="This is my hospital queue management application")
+
+app.include_router(patient_router)
