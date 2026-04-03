838b0e58a786befcde19d3c1d764db70202126e0
Sanskriti Choudhury <151363795+Sanskriti757@users.noreply.github.com>
2026-04-04 01:32:47 +0530

    Add User model with roles and attributes

---
+++ backend/app/models/user.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from enum import Enum
+
+from sqlalchemy import Column, Integer, String, Enum as SQLEnum, DateTime
+
+from app.database.connection import Base
+
+
+class User_Role(str,Enum):
+    ADMIN = "admin"
+    DOCTOR = "doctor"
+    TRIAGE= "triage"
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    name= Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(SQLEnum(User_Role), nullable=False)
+
+    department_id = Column(Integer, nullable=True)  # For doctors, this can be null for admins and triage nurses
+
+    created_at = Column(DateTime, nullable=False,default=datetime.utcnow)
