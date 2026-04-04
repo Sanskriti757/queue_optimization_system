9548727625ba1dadc3b1693a4ac2f4ddad006245
Muskaan Sharma <muskaansharmahm@gmail.com>
2026-04-04 12:40:11 +0530

    Enhance PatientModel with additional fields
    Added fields for disability and created_at, and improved formatting.

--- backend/app/models/patient.py
+++ backend/app/models/patient.py
@@ -1,13 +1,25 @@
+import datetime
+
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from app.database.connection import Base
+from sqlalchemy.orm import relationship
 
 # ye ek patient model hai jisme patient ki details store hongi database me
 class PatientModel(Base):
-    __tablename__="patients"
+    __tablename__ = "patients"
 
-    patient_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String,nullable=False)
+    patient_id = Column(Integer, primary_key=True, index=True)
+
+    name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
-    gender= Column(String, nullable=False)
-    contact_number = Column(String, nullable=False,index=True,)
+    gender = Column(String, nullable=False)
+
+    contact_number = Column(String, nullable=False)
     address = Column(String, nullable=True)
+
+    disability = Column(Boolean, default=False)
+
+    created_at = Column(DateTime)
+
+    # 🔗 Relationship
+    queues = relationship("QueueModel", back_populates="patient")
