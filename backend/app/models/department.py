da226ad1c09e5077981d26451b44a5c0c9f1c120
Muskaan Sharma <muskaansharmahm@gmail.com>
2026-04-04 12:38:22 +0530

    Add DepartmentModel for department management

---
+++ backend/app/models/department.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from sqlalchemy.orm import relationship
+from app.database.connection import Base
+
+
+
+class DepartmentModel(Base):
+    __tablename__ = "departments"
+    
+    department_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    code = Column(String, nullable=False, unique=True)
+    queues = relationship("QueueModel", back_populates="department")
