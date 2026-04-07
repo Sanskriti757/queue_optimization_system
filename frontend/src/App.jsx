e2d94c2eb262412d6514b1f0d0f797e56f9622d6
Ananyaa <your-email@example.com>
2026-04-07 19:24:44 +0530

    feat:complete setup of the frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+      {/* <Navbar />
+      <Dashboard /> */}
+      <h1>This is my app</h1>
+    </div>
+  )
+}
+
+export default App
