8cf81a824ecff2f10f35476c44864d3c86a55974
Muskaan Sharma <muskaansharmahm@gmail.com>
2026-04-08 19:47:01 +0530

    Enhance user schema with email validation and roles

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,5 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import Literal,Optional
+from app.models.user import UserRole
+
+class UserSchema(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: UserRole
+    department_id: Optional[int] = None
+
 
 class LoginSchema(BaseModel):
-    email: str
+    email: EmailStr
     password: str
+
+class UserResponseSchema(BaseModel):
+    user_id: int
+    name: str
+    role: UserRole
+
+class LoginResponseSchema(BaseModel):
+    access_token: str
+    token_type: str
+    user: UserResponseSchema
